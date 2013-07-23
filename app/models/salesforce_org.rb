2479690fc96c037137c7b9f98343554001255399
SergioGM <sergio@singletracksystems.com>
2013-07-23 16:09:07 +0100

    Hardcode encryption key

--- app/models/salesforce_org.rb
+++ app/models/salesforce_org.rb
@@ -1,8 +1,8 @@
 module StsSalesforceOrg
   class SalesforceOrg < ActiveRecord::Base
     attr_accessible :name, :packaged, :password, :sandbox, :token, :username, :disabled, :error_message
-    attr_encrypted :password, :attribute => 'password_encrypted', :key => Rails.Application.config.db_encryption_key
-    attr_encrypted :token,    :attribute => 'token_encrypted',    :key => Rails.Application.config.db_encryption_key
+    attr_encrypted :password, :attribute => 'password_encrypted', :key => 'JKdIKB1qgtsVT9wAAUHnlEbZ'#Rails.application.config.db_encryption_key
+    attr_encrypted :token,    :attribute => 'token_encrypted',    :key => 'JKdIKB1qgtsVT9wAAUHnlEbZ'#Rails.application.config.db_encryption_key
     #has_many :monitored_activities
 
     def disabled=(disabled)
