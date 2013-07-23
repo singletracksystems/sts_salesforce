934ac4557f7e47b6be3a1e4671fc1398ca2090c8
SergioGM <sergio@singletracksystems.com>
2013-07-23 11:52:38 +0100

    Fix file reference

--- lib/generators/sts_salesforce_org/install/install_generator.rb
+++ lib/generators/sts_salesforce_org/install/install_generator.rb
@@ -5,7 +5,7 @@ module StsSalesforceOrg
     class InstallGenerator < ::Rails::Generators::Base
       include Rails::Generators::Migration
 
-      source_root File.expand_path('../templates', __File__ )
+      source_root File.expand_path('../templates', __FILE__ )
 
       desc "add migrations"
 
