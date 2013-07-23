0bfed3bfc0f5a8fa7a6e4adc3611405699303c20
SergioGM <sergio@singletracksystems.com>
2013-07-23 15:52:32 +0100

    Add controller and route

--- app/models/salesforce_org.rb
+++ app/models/salesforce_org.rb
@@ -1,9 +1,9 @@
 module StsSalesforceOrg
   class SalesforceOrg < ActiveRecord::Base
     attr_accessible :name, :packaged, :password, :sandbox, :token, :username, :disabled, :error_message
-    attr_encrypted :password, :attribute => 'password_encrypted', :key => SingletrackMonitor::Application.config.db_encryption_key
-    attr_encrypted :token,    :attribute => 'token_encrypted',    :key => SingletrackMonitor::Application.config.db_encryption_key
-    has_many :monitored_activities
+    attr_encrypted :password, :attribute => 'password_encrypted', :key => Rails.Application.config.db_encryption_key
+    attr_encrypted :token,    :attribute => 'token_encrypted',    :key => Rails.Application.config.db_encryption_key
+    #has_many :monitored_activities
 
     def disabled=(disabled)
       write_attribute(:error_message, nil) if disabled == '0'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.Application.routes.draw do
+
+  get "testconnection" => "test_connection#test_conn"
+
+end
