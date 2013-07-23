8ff7755541a625b974827955e6de87a6f098737f
SergioGM <sergio@singletracksystems.com>
2013-07-23 16:41:46 +0100

    Back to Rails Generator

--- lib/generators/sts_salesforce_org/install/install_generator.rb
+++ lib/generators/sts_salesforce_org/install/install_generator.rb
@@ -1,12 +1,22 @@
-require 'rails/generators/active_record'
+require 'rails/generators/migration'
 
 module StsSalesforceOrg
   module Generators
-    class InstallGenerator < ActiveRecord::Generators::Base
+    class InstallGenerator < ::Rails::Generators::Base
+      include Rails::Generators::Migration
 
       source_root File.expand_path('../templates', __FILE__ )
 
       desc "add migrations"
+
+      def self.next_migration_number(path)
+        unless @prev_migration_nr
+          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        else
+          @prev_migration_nr += 1
+        end
+        @prev_migration_nr.to_s
+      end
 
       def copy_migrations
         copy_migration "create_salesforce_org"
