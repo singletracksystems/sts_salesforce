cc321494762b6daee1054dccc1d33b613f12512d
SergioGM <sergio@singletracksystems.com>
2013-07-24 16:41:14 +0100

    Remove next_migration since using ActiveRecord

--- lib/generators/sts_salesforce/install/install_generator.rb
+++ lib/generators/sts_salesforce/install/install_generator.rb
@@ -11,15 +11,6 @@ module StsSalesforce
 
       desc "add migrations"
 
-      def self.next_migration_number(path)
-        unless @prev_migration_nr
-          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
-        else
-          @prev_migration_nr += 1
-        end
-        @prev_migration_nr.to_s
-      end
-
       def copy_migrations
         copy_migration "create_salesforce_orgs"
         copy_migration "add_disabled_to_salesforce_org"
