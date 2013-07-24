183e21d42071809d887f200d50ed7be1393ce907
SergioGM <sergio@singletracksystems.com>
2013-07-24 10:55:27 +0100

    Fix misspelt filename

--- lib/generators/sts_salesforce_org/install/install_generator.rb
+++ lib/generators/sts_salesforce_org/install/install_generator.rb
@@ -21,7 +21,7 @@ module StsSalesforceOrg
       end
 
       def copy_migrations
-        copy_migration "create_salesforce_org"
+        copy_migration "create_salesforce_orgs"
         copy_migration "add_disabled_to_salesforce_org"
         copy_migration "add_error_message_to_salesforce_org"
         copy_migration "expand_error_message_field_length"
