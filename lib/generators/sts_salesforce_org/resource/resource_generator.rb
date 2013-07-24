83c86f51335547e92d000b40e0698c5ab69e0ea2
SergioGM <sergio@singletracksystems.com>
2013-07-24 09:42:23 +0100

    Remove module from model and controller

--- lib/generators/sts_salesforce_org/resource/resource_generator.rb
+++ lib/generators/sts_salesforce_org/resource/resource_generator.rb
@@ -1,10 +1,10 @@
 module StsSalesforceOrg
   module Generators
-    class ResourceGenerator < Rails::Generators::NamedBase
+    class ResourceGenerator < Rails::Generators::Base
 
       source_root File.expand_path("../templates", __FILE__)
 
-      def generate_config_file
+      def generate_activeadmin_tab
         template "salesforce_orgs.rb", "app/admin/salesforce_orgs.rb"
       end
 
