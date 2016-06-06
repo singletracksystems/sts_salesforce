9ea94a195047ef2a43dbb3bfeb8ea35acdb735e6
Dave Fish <dave@singletrack.com>
2016-06-06 15:56:26 +0100

    Updated default_scope syntax for Rails 4.2

--- app/models/salesforce_org.rb
+++ app/models/salesforce_org.rb
@@ -1,5 +1,5 @@
 class SalesforceOrg < ActiveRecord::Base
-  default_scope order('name Asc')
+  default_scope { order('name Asc') }
   attr_accessible :name, :packaged, :password, :sandbox, :token, :username, :disabled, :error_message, :organization_id, :email_addresses, :client_id, :client_secret
 
   attr_encrypted :username, :attribute => 'username_encrypted', :key => (Rails.application ? Rails.application.config.db_encryption_key : 'r0-wfojomLCbsLqYxDBsHjnEDEMWTo')
